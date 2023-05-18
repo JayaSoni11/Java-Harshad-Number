61a132b7c2d4c3f8663ab47cf3a1ce6497aa3f6f
HimanshuGoyal31 <himanshugoyal3104@gmail.com>
2023-05-18 10:58:58 +0530

    Java-Harshad-Number

---
+++ HarshadNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class HarshadNumber
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        int store = num;
+        int sum = 0;
+        
+        while(num > 0){
+            int temp = num % 10;
+            sum = sum + temp;
+            num = num/10;
+        }
+        
+        if(store % sum == 0)
+            System.out.println("Number is Harshad");
+        else
+            System.out.println("Not a Harshad Number");
+	}
+}
