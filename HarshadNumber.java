0c3f5ffce5dc1298528b912c89e0420e61b80b90
JayaSoni11 <109737132+JayaSoni11@users.noreply.github.com>
2023-05-18 15:13:55 +0530

    Update HarshadNumber.java

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -10,7 +10,6 @@ public class HarshadNumber
         
         int store = num;
         int sum = 0;
-        
         while(num > 0){
             int temp = num % 10;
             sum = sum + temp;
