0944b9562b25bdf44d5978e1ee44394fab5497cf
HimanshuGoyal31 <himanshugoyal3104@gmail.com>
2023-05-18 12:17:41 +0530

    drop comments

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
+
 public class HarshadNumber
 {
+    //start main function
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
